07f3308cc14e936c96ab1558b7bbef276a9ca874
Megan Hagenauer <hagenaue@umich.edu>
2016-12-19 16:29:06 -0500

    Create VariablesToInclude.R

---
+++ VariablesToInclude.R
@@ -0,0 +1,7 @@
+BrainRegion=c("ACG", "ANTHAL", "AMY", "CB", "DLPFC", "HC", "MTHAL", "NACC", "PCG", "SCG")
+AmygdalaSubRegion=c("AAA", "AB", "AHA", "Basal", "Central", "CO", "Lateral", "Medial", "PAC", "PL")
+Platform=c("Affymetrix", "Illumina", "RNA.Seq")
+Dataset=c("Affy", "Freeze3", "Pritzker960", "LCM")
+VariableOfInterest=c("BP", "MDD", "SCHIZ", "BrainPH", "AgonalFactor", "PMI",  "Gender", "Age", "RNAIntegrity")
+Output=c("Beta", "DirectionOfEffect", "Tstat", "NominalPvalue", "PercentileRank", "ProbeAnnotation", "FDR")
+
